ccc50b0cc7f310dbd513de087aef9b63325cc070
wang <104444813+wangyuheng115@users.noreply.github.com>
2022-10-21 12:41:34 +0200

    usa ajax e js

--- login.php
+++ login.php
@@ -9,15 +9,23 @@ session_start();
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Game_store_Loginpage</title>
 	<link rel="stylesheet" href="css/stile_login_registra.css" media="screen">
+	<script src="js/login_view_pw.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 </head>
 <body>
 	<h1 align="center">Game<span>Store</span></h1>
-	<div class="box">
-		
+	<div class="box">	
 		<form action="db/use_db.php" method="post" enctype="application/x-www-form-urlencoded">
 			<div class="datiuser">
-				Email<input type="email" name="Email" value="" required="required" ><br>
-				Password<input type="password" name="Password" value="" required="required" ><br><br>
+				<div class="email">
+				Email<input type="email" name="Email" id="email" required="required" ><br>
+				<span id="controll_email" style="font-size: 18px;"></span>
+				</div>
+
+				<div class="pw">
+				Password<input type="password" name="Password" id="password" required="required" ><img src="img/no_pw.png" id="view_pw" width="25px" onclick="VisualizzaPassword()" style="position:relative; top: 10px;"><br>
+				<span id="controll_password" style="font-size: 18px;"></span>
+				</div>
 			</div>
 			<?php
 			if(isset($_GET['msg'])){
@@ -26,9 +34,44 @@ session_start();
 			?>
 			<input type="submit" name="login" value="Login" class="btn">
 			<input type="submit" onclick="window.location.href='registra.php'" value="Registra"class="btn">
-			
 		</form>
-
 	</div>
+
+	<script>
+		$(document).ready(function (){
+    		$(".box").fadeIn(2500);
+		})
+
+
+		$('#email').blur(function(){
+			 var email = $('#email').val();
+			
+			 $.post("./db/Verifica_email.php",{Email: email},function(data){
+				if(data.trova === 0){
+					var email = document.querySelectorAll("input");
+					email[0].style.border = "solid 2px #f00";
+					document.getElementById("controll_email").innerHTML = "Email non esiste!";
+				}
+				else{
+						document.getElementById("controll_email").innerHTML = "<font color=#53E652>Email valido</font>";
+						var email = document.querySelectorAll("input");
+						email[0].style.border = "solid 2px #53E652";
+				}
+			 },"json");
+		});	
+
+		$('#password').blur(function(){
+			 var pw = $('#password').val();
+			
+			 $.post("./db/Verifica_pw.php",{Password: pw},function(data){
+				if(data.trova === 0){
+					document.getElementById("controll_password").innerHTML = "Password errato!";
+				}
+				else{
+					document.getElementById("controll_password").innerHTML = "";
+				}
+			 },"json");
+		});	
+	</script>
 </body>
 </html>
