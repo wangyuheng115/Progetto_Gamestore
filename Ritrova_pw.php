3f37f1eab91ba6098a521163604c38c0b6018e9b
wang <wangyuheng115@gmail.com>
2022-10-28 12:02:18 +0200

    Aggiugnto jquery ui plugin

--- Ritrova_pw.php
+++ Ritrova_pw.php
@@ -4,6 +4,7 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,61 +12,63 @@ session_start();
 	<link rel="stylesheet" href="css/stile_login.css" media="screen">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 	<link href="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.bundle.min.js"></script>
 </head>
-<body>
-<div class="container p-5 my-5 text-white">
-	<div class="box" id="box">
-		<div class="row">
-			<div class="col">
-				<h1 align="center"><img src="img/logo.png" width="50px" title="GameStore">Ritrova Password</h1>
-			</div>
-		</div>
 
-		<div class="row">
-			<div class="col-sm-6 offset-sm-3 col-xxl-6 offset-xxl-3 col-xl-6 offset-xl-3">
-				<form action="email.php" method="post" enctype="application/x-www-form-urlencoded">
-					<div class="form-floating ">
-						<input type="email" name="Email" id="email" required="required" class="form-control" placeholder="Inserisci Email" style="border: 0; border-bottom:2px solid black ; background: rgba(0 ,0 , 0, 0%);">
-						<label for="email">Email</label>
-					</div>
-					<p id="controll_email" style="font-size: 18px;" align="center"></p>
+<body>
+	<div class="container p-5 my-5 text-white">
+		<div class="box" id="box">
+			<div class="row">
+				<div class="col">
+					<h1 align="center"><img src="img/logo.png" width="50px" title="GameStore">Ritrova Password</h1>
+				</div>
 			</div>
-					<div class="col-sm-3 p-2 col-xxl-3 col-xl-3" id="buttone">
-						<input type="submit" name="trova" value="Commit" class="btn btn-primary" id="buttone"/>
-					</div>
-					
-						
+
+			<div class="row">
+				<div class="col-sm-6 offset-sm-3 col-xxl-6 offset-xxl-3 col-xl-6 offset-xl-3">
+					<form action="email.php" method="post" enctype="application/x-www-form-urlencoded">
+						<div class="form-floating ">
+							<input type="email" name="Email" id="email" required="required" class="form-control" placeholder="Inserisci Email" style="border: 0; border-bottom:2px solid black ; background: rgba(0 ,0 , 0, 0%);">
+							<label for="email">Email</label>
+						</div>
+						<p id="controll_email" style="font-size: 18px;" align="center"></p>
+				</div>
+				<div class="col-sm-3 p-2 col-xxl-3 col-xl-3" id="buttone">
+					<input type="submit" name="trova" value="Commit" class="btn btn-primary" id="buttone" />
+				</div>
+
+
 				</form>
-			
+
+			</div>
 		</div>
 	</div>
-</div>
 	<script>
-		$(document).ready(function (){
-    		$("#box").fadeIn(2500);
+		$(document).ready(function() {
+			$("#box").fadeIn(2500);
 		})
 
-		$('#email').mouseleave(function(){
-			 var email = $('#email').val();
-			if(email === ""){
+		$('#email').mouseleave(function() {
+			var email = $('#email').val();
+			if (email === "") {
 				document.getElementById("controll_email").innerHTML = ("<div class='alert alert-primary mt-3'> <strong>Inserisci</strong> il tuo email!</div>");
-			}
-			else{
-			 $.post("./db/Verifica_email.php",{Email: email},function(data){
-				if(data.trova === 1){
-					var email = document.querySelectorAll("input");
-					email[0].style.border = "solid 2px #53E652";
-					document.getElementById("controll_email").innerHTML = ("<div class='alert alert-success mt-3'> <strong>Email</strong> valido!</div>");
-				}
-				else{
+			} else {
+				$.post("./db/Verifica_email.php", {
+					Email: email
+				}, function(data) {
+					if (data.trova === 1) {
+						var email = document.querySelectorAll("input");
+						email[0].style.border = "solid 2px #53E652";
+						document.getElementById("controll_email").innerHTML = ("<div class='alert alert-success mt-3'> <strong>Email</strong> valido!</div>");
+					} else {
 						document.getElementById("controll_email").innerHTML = ("<div class='alert alert-danger mt-3'> <strong>Email</strong> non esiste!</div>");
 						var email = document.querySelectorAll("input");
 						email[0].style.border = "solid 2px #f00";
-				}
-			 },"json");
+					}
+				}, "json");
 			}
-		});	
+		});
 	</script>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -4,32 +4,87 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Game_store_Loginpage</title>
-	<link rel="stylesheet" href="css/stile_login.css" media="screen">
-	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/css/bootstrap.min.css">     
-	<script src="js/login_view_pw.js"></script>
-	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-	<script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.bundle.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Game_store_Loginpage</title>
+    <link rel="stylesheet" href="css/stile_login.css" media="screen">
+    <link rel="stylesheet" href="css_bootstrap/bootstrap.css">
+    <!-- <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/css/bootstrap.min.css">  -->
+    <script src="js/login_view_pw.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <!-- <script src="https://cdn.staticfile.org/twitter-bootstrap/5.1.1/js/bootstrap.bundle.min.js"></script> -->
+
+    <link href="jquery-ui-plugin/css/base/jquery-ui-1.10.4.custom.css" rel="stylesheet">
+    <script src="jquery-ui-plugin/js/jquery-1.10.2.js"></script>
+    <script src="jquery-ui-plugin/js/jquery-ui-1.10.4.custom.js"></script>
+
+    <script>
+        $(function() {
+            $(document).tooltip({
+                position: {
+                    my: "center top-30",
+                    at: "center top",
+                    using: function(position, feedback) {
+                        $(this).css(position);
+                        $("<div>")
+                            .addClass("tooltip")
+                            .addClass(feedback.vertical)
+                            .addClass(feedback.horizontal)
+                            .appendTo(this);
+                    }
+                }
+            });
+        });
+    </script>
+
+    <style>
+        .ui-tooltip,
+        .tooltip:after {
+            background: white;
+            border: 2px solid black;
+        }
+
+        .ui-tooltip {
+            padding: 10px 20px;
+            color: black;
+            border-radius: 20px;
+            font: bold 14px "Helvetica Neue", Sans-Serif;
+            text-transform: capitalize;
+            box-shadow: 0 0 7px lightgray;
+        }
+
+        .tooltip {
+            width: 30px;
+            height: 20px;
+            overflow: hidden;
+            position: absolute;
+            left: 50%;
+            margin-left: -35px;
+            bottom: -16px;
+        }
+    </style>
 </head>
+
 <body>
-    
-	<div class="container p-5 my-5 text-white" id="container">
-    <div class="box" id="box">
-        <div class="row">
-            <div class="col-12"><h1 align="center"><img src="img/logo.png" width="50px" title="GameStore">Game<span style="color: red;">Store</span></h1></div>
-        </div>
-        <div class="row">
-            <div class="col-12">   	
+
+    <div class="container p-5 my-5 text-white" id="container">
+        <div class="box" id="box">
+            <div class="row">
+                <div class="col-12">
+                    <h1 align="center"><img src="img/logo.png" width="50px" title="GameStore">Game<span style="color: red;">Store</span></h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
                     <form action="db/use_db.php" method="post" enctype="application/x-www-form-urlencoded">
                         <div class="row">
                             <div class="col-xxl-6 offset-xxl-3 col-xl-6 offset-xl-3 col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3 col-sm-8 offset-sm-2">
                                 <div class="email" align="center">
                                     <div class="form-floating ">
-                                        <input type="email" name="Email" id="email" required="required" class="form-control form-control-sm" placeholder="Inserisci Email" style="border: 0; border-bottom:2px solid black ; background: rgba(0 ,0 , 0, 0%);">
+                                        <input type="email" title="Inserisci il tuo email!" name="Email" id="email" required="required" class="form-control form-control-sm" placeholder="Inserisci Email" style="border: 0; border-bottom:2px solid black ; background: rgba(0 ,0 , 0, 0%);">
                                         <label for="email">Email</label>
                                     </div>
                                     <span id="controll_email" style="font-size: 18px;"></span>
@@ -38,23 +93,23 @@ session_start();
                         </div><br>
 
                         <div class="row">
-                            <div class="col-xxl-6 offset-xxl-3 col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3 col-sm-8 offset-sm-2">  
+                            <div class="col-sm-6 offset-sm-3">
                                 <div class="pw" align="center">
                                     <div class="form-floating ">
-                                        <input type="password" name="Password" id="password" required="required" class="form-control form-control-sm" placeholder="Inserisci Password" style="border: 0; border-bottom:2px solid black ; background: rgba(0 ,0 , 0, 0%);">
+                                        <input type="password" name="Password" title="Inserisci la password!" id="password" required="required" class="form-control form-control-sm" placeholder="Inserisci Password" style="border: 0; border-bottom:2px solid black ; background: rgba(0 ,0 , 0, 0%);">
                                         <label for="password">Password</label>
                                         <div class="img"><img src="img/no_pw.png" id="view_pw" width="25px" onclick="VisualizzaPassword()"></div>
                                     </div>
                                     <span id="controll_password" style="font-size: 18px;"></span>
                                 </div>
-                            </div>                              
-                        </div>    
-                        
-                                    <?php
-                                    if(isset($_GET['msg'])){
-                                            echo"<p style='color: red;' align='center'>Hai sbagliato Email o Password.</p>";
-                                        }
-                                    ?>
+                            </div>
+                        </div>
+
+                        <?php
+                        if (isset($_GET['msg'])) {
+                            echo "<p style='color: red;' align='center'>Hai sbagliato Email o Password.</p>";
+                        }
+                        ?>
                         <div class="row">
                             <div class="col-12">
                                 <div class="Ritrova" align="center">
@@ -67,50 +122,53 @@ session_start();
                             <div class="col-12">
                                 <div class="buttone" align="center">
                                     <input type="submit" name="login" value="Login" class="btn btn-primary btn-lg" align="center">
-                                    <input type="submit" onclick="window.location.href='registra.php'" value="Registra"class="btn btn-primary btn-lg">
+                                    <input type="submit" onclick="window.location.href='registra.php'" value="Registra" class="btn btn-primary btn-lg">
                                 </div>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-	</div>
+        </div>
     </div>
-	<script>
-		$(document).ready(function (){
-    		$("#box").fadeIn(2500);
-		})
+    <script>
+        $(document).ready(function() {
+            $("#box").fadeIn(2500);
+        })
 
 
-		$('#email').blur(function(){
-			 var email = $('#email').val();
-			
-			 $.post("./db/Verifica_email.php",{Email: email},function(data){
-				if(data.trova === 0){
-					var email = document.querySelectorAll("input");
-					email[0].style.border = "solid 2px #f00";
-					document.getElementById("controll_email").innerHTML = "<font color=#f00>Email non esiste!";
-				}
-				else{
-						document.getElementById("controll_email").innerHTML = "<font color=#53E652>Email valido</font>";
-						var email = document.querySelectorAll("input");
-						email[0].style.border = "solid 2px #53E652";
-				}
-			 },"json");
-		});	
+        $('#email').blur(function() {
+            var email = $('#email').val();
 
-		$('#password').blur(function(){
-			 var pw = $('#password').val();
-			
-			 $.post("./db/Verifica_pw.php",{Password: pw},function(data){
-				if(data.trova === 0){
-					document.getElementById("controll_password").innerHTML = "<font color=#f00>Password errato!";
-				}
-				else{
-					document.getElementById("controll_password").innerHTML = "";
-				}
-			 },"json");
-		});	
-	</script>
+            $.post("./db/Verifica_email.php", {
+                Email: email
+            }, function(data) {
+                if (data.trova === 0) {
+                    var email = document.querySelectorAll("input");
+                    email[0].style.border = "solid 2px #f00";
+                    document.getElementById("controll_email").innerHTML = "<font color=#f00>Email non esiste!";
+                } else {
+                    document.getElementById("controll_email").innerHTML = "<font color=#53E652>Email valido</font>";
+                    var email = document.querySelectorAll("input");
+                    email[0].style.border = "solid 2px #53E652";
+                }
+            }, "json");
+        });
+
+        $('#password').blur(function() {
+            var pw = $('#password').val();
+
+            $.post("./db/Verifica_pw.php", {
+                Password: pw
+            }, function(data) {
+                if (data.trova === 0) {
+                    document.getElementById("controll_password").innerHTML = "<font color=#f00>Password errato!";
+                } else {
+                    document.getElementById("controll_password").innerHTML = "";
+                }
+            }, "json");
+        });
+    </script>
 </body>
+
 </html>
